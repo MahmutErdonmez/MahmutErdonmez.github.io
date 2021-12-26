dc36275e5337c5cefc74313f807db4d0418bef6f
MahmutErdonmez <71981967+MahmutErdonmez@users.noreply.github.com>
2021-12-27 00:59:04 +0300

    Add files via upload

---
+++ vue.js
@@ -0,0 +1,29 @@
+new Vue({
+  el: '#app',
+  data () {
+    return {
+      shortLink: null,
+      longLink: null
+      
+    }
+  },
+  methods:{
+    shorten() {
+      if (this.longLink == null || this.longLink == undefined) {
+        return;
+      }     
+      const article = { long_url: this.longLink };
+      const headers = { 
+        "Authorization": "Bearer 02699c9ce01c7925ac6d693f779743f78383cf3c",
+        'Content-Type': 'application/json'
+      };
+      axios.post("https://api-ssl.bitly.com/v4/shorten", article, { headers })
+        .then((response) =>{          
+            this.shortLink = response.data["id"];            
+          
+        }).catch((error) => {
+          alert(error)  }) 
+    }
+  }
+
+})
